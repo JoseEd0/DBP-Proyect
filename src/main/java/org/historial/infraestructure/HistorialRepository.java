186ba866ab29ced905ede19eb26effb6e192555c
ErickExploer <erick.tataje@utec.edu.pe>
2024-06-01 02:37:01 -0500

    unu

--- src/main/java/org/historial/infraestructure/HistorialRepository.java
+++
@@ -1,9 +0,0 @@
-package org.historial.infraestructure;
-
-import org.historial.domain.Historial;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HistorialRepository extends JpaRepository<Historial, Long> {
-}
